e3b85d9ae8bb13a41b6a7c85cdf36d1616e78eb0
Raj <rajshivage@gmail.com>
2022-07-24 13:38:12 -0400

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+pod_subnet = "10.244.0.0/16"
+service_subnet = "10.96.0.0/12"
+api_server_address = "127.0.0.1"
+api_server_port = 6443
--- variables.tf
+++ variables.tf
@@ -1,21 +1,18 @@
 variable "pod_subnet" {
     type        = string
     description = "Subnet CIDR for pod networking"
-    default     = "10.244.0.0/16"
     nullable    = false    
 }
 
 variable "service_subnet" {
     type        = string
     description = "Subnet CIDR for k8s services"
-    default     = "10.96.0.0/12"
     nullable    = false    
 }
 
 variable "api_server_address" {
     type        = string
     description = "API server listening address"
-    default     = "127.0.0.1"
     nullable    = false  
 }
 
