e0bbffe272528ee459cbb5bf654a93186bfd713d
rajshivage <rajshivage@gmail.com>
2022-07-25 09:16:50 +0000

    Upload ArgoCD app of apps changes

--- main.tf
+++ main.tf
@@ -46,3 +46,19 @@ resource "kind_cluster" "default" {
     }
   }
 }
+
+# Install ArgoCD
+resource "helm_release" "argocd" {
+  name  = "argocd"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "4.10.0"
+  create_namespace = true
+  cleanup_on_fail  = true
+
+  values = [
+    file("argocd/application.yaml")
+  ]
+}
